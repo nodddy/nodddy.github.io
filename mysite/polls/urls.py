46b6aa12212c52991f3e3cbf28c30f4fce5ae7e4
julia-moeller <71017640+julia-moeller@users.noreply.github.com>
2021-04-13 18:16:52 +0200

    delete

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name='polls'
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:experiment_id>/', views.detail, name='detail'),
-    path('<int:experiment_id>/add_parameter', views.add_parameter, name='add_parameter'),
-    path('<int:experiment_id>/add_step', views.add_data_to_model, name='add_data_to_model'),
-    path('<int:experiment_id>/add_note', views.add_data_to_model, name='add_data_to_model')
+    path('<int:experiment_id>/<form_id>/add', views.add, name='add'),
+    path('<int:experiment_id>/<form_id>/delete', views.delete, name='delete'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .forms import ParameterForm, NoteForm, StepForm
 from .models import Parameter, Experiment
@@ -23,25 +25,32 @@ def detail(request, experiment_id):
     return render(request, 'polls/detail.html', context)
 
 
-
-def add_parameter(request, experiment_id, modelform):
-    return add_data_to_model(request, experiment_id, modelform)
-
-def add_data_to_model(request, experiment_id, modelform):
-    modeldict = {
-        'parameter': ParameterForm(request.POST),
-        'note': NoteForm(request.POST),
-        'step': StepForm(request.POST),
-    }
+def add(request, experiment_id, form_id=''):
     experiment = get_object_or_404(Experiment, pk=experiment_id)
-    form = modeldict[modelform]
+    form_dict = {
+        'step_form': StepForm,
+        'note_form': NoteForm,
+        'parameter_form': ParameterForm
+    }
+    form = form_dict[form_id](request.POST)
+    if 'Abort' in request.POST.keys():
+        return HttpResponseRedirect(reverse('polls:detail', args=(experiment.id,)))
     if form.is_valid():
         new_parameter = form.save(commit=False)
         new_parameter.experiment_id = experiment_id
         new_parameter.save()
-    if 'Abort' in request.POST.keys():
-        return render(request, 'polls/detail.html', {'experiment': experiment})
+    form = form_dict[form_id]()
+    return render(request, 'polls/add.html', {'form_id': form_id,
+                                              'form': form,
+                                              'experiment': experiment,
+                                              'block_add': True})
 
-    return render(request, f'polls/add_{modelform}_form.html', {'form': form,
-                                                                'experiment': experiment,
-                                                                'block_add': True})
+
+def delete(request, experiment_id, form_id):
+    print(form_id)
+    pk = form_id
+    experiment = get_object_or_404(Experiment, pk=experiment_id)
+    if request.method == 'POST' and 'delete' in request.POST:
+        pass
+    # experiment.object.filter().delete()
+    return HttpResponseRedirect(reverse('polls:detail', args=(experiment.id,)))
